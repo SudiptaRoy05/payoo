119d7c3b945278ef7719973db07e89b32b5b8266
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-19 00:48:47 +0600

    add money added

---
+++ js/home.js
@@ -0,0 +1,19 @@
+document.getElementById('addMoney').addEventListener('click', function (e) {
+    e.preventDefault();
+    const pin = document.getElementById('passwd').value;
+    if(pin === '1234'){
+        const amount = document.getElementById('inputMoney').value;
+
+        // console.log(amount)
+        const availableBlns = document.getElementById('availableBalance').innerText;
+        const cb = parseFloat(availableBlns);
+        // console.log(cb);
+        const a = parseFloat(amount);
+        // console.log(a)
+        const newBalance = cb + a
+       
+        
+        document.getElementById('availableBalance').innerText = newBalance;
+        
+    }
+})
