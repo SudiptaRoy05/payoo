b7b6f87a6ec53b6d9a76dee2353cf91caaaeff5e
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-19 00:13:56 +0600

    home added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 // console.log("first")
 
+// step1: event listener setup
 document.getElementById('btnLogin').addEventListener('click', function (e) {
-    e.preventDefault(); 
+    e.preventDefault();
+
+    // step2:get phone num and pin
     const num = document.getElementById('pNumber').value;
-    console.log(num);
+    const pin = document.getElementById('passwd').value;
+    // step3: validate phone and pin 
+    if (num === '123456' && pin === '1234') {
+        window.location.href = './home.html'
+    }
+    else {
+        alert('wrong phone number or pin');
+    }
 })
