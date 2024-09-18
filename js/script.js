d86e9fcb067bc5edacf5eb4d4214598bab5914da
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-18 23:20:26 +0600

    start adding dom and preventdefault function added

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// console.log("first")
+
+document.getElementById('btnLogin').addEventListener('click', function (e) {
+    e.preventDefault(); 
+    const num = document.getElementById('pNumber').value;
+    console.log(num);
+})
